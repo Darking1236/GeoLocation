2296c88b9d6e7ec3e9a73ae9c9d6c4bb8f82559c
daniel alejandro rodriguez solis <dany.15.dars@gmail.com>
2022-02-24 20:44:57 -0800

    scrip asistente virtual

---
+++ proyecto.py
@@ -0,0 +1,35 @@
+from warnings import catch_warnings
+import speech_recognition as sr
+import pyttsx3
+
+
+listener= sr.Recognizer()
+name='alexa'
+engine = pyttsx3.init()
+
+def hablar(text):   
+    engine.say(text)
+    engine.runAndWait()
+
+def listen():
+    try: 
+        with sr.Microphone() as source:
+            print("escuchando...")
+            voice= listener.listen(source)
+            rec=listener.recognize_google(voice)
+            record= rec.lower()
+            if name in record:
+                print(record)
+            
+    except:
+        pass
+    return record
+
+def run():
+    recording=listen()
+    if 'ejecuta' in recording:
+        hablar('Analizando')
+    else:
+        hablar('no te entiendo,perdon')
+
+run()
